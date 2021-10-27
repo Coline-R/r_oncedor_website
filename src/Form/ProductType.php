be2ce42a6a8e5578bfb7c65353a3037b95518ad2
C. R <84263228+Coline-R@users.noreply.github.com>
2021-10-27 20:29:15 +0200

    feat : create product form

---
+++ src/Form/ProductType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => "Nom"
+            ])
+            ->add('description', TextareaType::class)
+            ->add('summary', TextareaType::class)
+            ->add('price', NumberType::class )
+            ->add('tome', NumberType::class)
+            ->add('picture', FileType::class)
+            ->add('type', ChoiceType::class)
+            ->add('category', ChoiceType::class)
+            ->add('edition', ChoiceType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
