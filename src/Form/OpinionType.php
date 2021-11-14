058d3240d59f74dfa62bc5cf1abc261a5e8b4954
C. R <84263228+Coline-R@users.noreply.github.com>
2021-11-14 15:20:07 +0100

    feat : update opinion form and view

--- src/Form/OpinionType.php
+++ src/Form/OpinionType.php
@@ -6,6 +6,7 @@ use App\Entity\Opinion;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -16,13 +17,25 @@ class OpinionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('opinion', TextareaType::class)
-            ->add('author', TextType::class)
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'attr' => ['placeholder' => 'Titre de l\'avis']
+            ])
+            ->add('opinion', TextareaType::class, [
+                'label' => 'Avis',
+                'attr' => ['placeholder' => 'L\'avis lecteur']
+            ])
+            ->add('author', TextType::class, [
+                'label' => 'Auteur',
+                'attr' => ['placeholder' => 'L\'auteur de l\'avis']
+            ])
             ->add('product', EntityType::class, [
                 'label' => 'Produit',
                 'class' => Product::class,
                 'choice_label' => 'name' 
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer'
             ])
         ;
     }
