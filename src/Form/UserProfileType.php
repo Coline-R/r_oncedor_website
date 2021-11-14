78bb655b05e8a148899c3d9d50004d891046d309
C. R <84263228+Coline-R@users.noreply.github.com>
2021-11-14 15:46:10 +0100

    feat : update user profile form and view

--- src/Form/UserProfileType.php
+++ src/Form/UserProfileType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,10 +15,25 @@ class UserProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('lastName', TextType::class)
-            ->add('firstName', TextType::class)
-            ->add('phoneNumber', TextType::class)
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => ['placeholder' => 'Votre prénom']
+                ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['placeholder' => 'Votre nom']
+                ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Numéro de téléphone',
+                'attr' => ['placeholder' => 'Votre numéro de téléphone']
+                ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['placeholder' => 'Votre adresse email']
+                ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ])
         ;
     }
 
